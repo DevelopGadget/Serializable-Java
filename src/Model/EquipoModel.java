8ede9ff2b5648a92bc9f92df5c9dabbe24ebd58e
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-20 18:46:03 -0500

    Equipo Modelo

--- src/Model/EquipoModel.java
+++ src/Model/EquipoModel.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model;
 
-/**
- *
- * @author FernanPro18
- */
+import java.net.URI;
+
 public class EquipoModel {
     
+    private String sNombre;
+    private String sEstadio;
+    private URI uEstadio;
+    private URI uEscudo;
+
+    public EquipoModel(String sNombre, String sEstadio, URI uEstadio, URI uEscudo) {
+        this.sNombre = sNombre;
+        this.sEstadio = sEstadio;
+        this.uEstadio = uEstadio;
+        this.uEscudo = uEscudo;
+    }
+
+    public void setsNombre(String sNombre) {
+        this.sNombre = sNombre;
+    }
+
+    public void setsEstadio(String sEstadio) {
+        this.sEstadio = sEstadio;
+    }
+
+    public void setuEstadio(URI uEstadio) {
+        this.uEstadio = uEstadio;
+    }
+
+    public void setuEscudo(URI uEscudo) {
+        this.uEscudo = uEscudo;
+    }
 }
