3275d77d03e8e8277cae7a64df53f5fbfab87e69
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-21 09:23:03 -0500

    serializable estructura

--- src/serializable/Serializable.java
+++ src/serializable/Serializable.java
@@ -1,7 +1,46 @@
 package serializable;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class Serializable {
-    
-    
-    
+
+    private File fichero;
+    private FileInputStream archivo;
+    private ObjectInputStream obj_archivo;
+    private FileOutputStream persisten;
+    private ObjectOutputStream obj_persiten;
+
+    public Object cargar_datos(String url) {
+        fichero = new File(url);
+        if (fichero.exists()) {
+            try {
+                archivo = new FileInputStream(url);
+                obj_archivo = new ObjectInputStream(archivo);
+                return obj_archivo.readObject();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Ha ocurrido un error con el archivo");
+                return false;
+            }
+        } else {
+            return true;
+        }
+
+    }
+
+    private void Escribir(String Url, ArrayList Array) {
+        try {
+            persisten = new FileOutputStream(Url);
+            obj_persiten = new ObjectOutputStream(persisten);
+            obj_persiten.writeObject(Array);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ha ocurrido un error",  "Error", 0);
+        }
+    }
+
 }
