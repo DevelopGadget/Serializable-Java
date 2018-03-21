84d113e81a5846462ec14da68a45d87e7968ce1e
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-20 20:09:51 -0500

    val url

--- src/Controller/EquipoController.java
+++ src/Controller/EquipoController.java
@@ -1,36 +1,45 @@
-
 package Controller;
 
 import Model.EquipoModel;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
 
 public class EquipoController {
-    
+
     private ArrayList Equipos = new ArrayList<EquipoModel>();
 
     public EquipoController() {
     }
 
-    public void Post(EquipoModel Equipo){
+    public void Post(EquipoModel Equipo) {
         Equipos.add(Equipo);
     }
-    
-    public void Get(String Filter){
-        
+
+    public void Get(String Filter) {
+
     }
-    
-    public void Put(int Index, EquipoModel Equipo){
+
+    public void Put(int Index, EquipoModel Equipo) {
         Equipos.set(Index, Equipo);
     }
-    
-    public void Delete(int Index){
+
+    public void Delete(int Index) {
         Equipos.remove(Index);
     }
-    
+
+    public boolean ValUrl(String Url) {
+        try {
+            ImageIO.read(new URL(Url));
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
     public ArrayList getEquipos() {
         return Equipos;
     }
-    
-    
-    
+
 }
